55f9cbfbf1f40f775ef91a9d9189ca7d7abd1c05
Stefan Butz <stefan260699@gmail.com>
2022-01-03 12:29:49 +0100

    wine regression: clean up

--- wine/wine_regression.py
+++ wine/wine_regression.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import pandas.plotting
-from sklearn.model_selection import train_test_split
+from pandas.plotting import scatter_matrix
 from sklearn.linear_model import LinearRegression, BayesianRidge
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import graphviz as gv
+import matplotlib.pyplot as plt
+import pandas as pd
+
+""" TODO:
+- df: red vs white wine
+- train_test_split: different random_states
+- train_test_split: limit features (x_columns_2)
+- tree: try different params 
+- use neural networks
+- plot: r2 depending on params?
+- more evaluation numbers beside r2?
+- use scaler?
+"""
 
 df = pd.read_csv('./wine/winequality-red.csv', sep=';', header=0)
 
@@ -22,51 +33,99 @@ y_column = ["quality"]
 
 print(df)
 
-# Scatter matrix
-
-scatter = pd.plotting.scatter_matrix(df[x_columns + y_column],
-                                     figsize=(15, 15),
-                                     marker='o',
-                                     c=df['quality'].values,
-                                     s=30,
-                                     alpha=0.8,
-                                     )
-
+""" Scatter matrix """
+scatter_matrix(df[x_columns + y_column],
+               figsize=(15, 15),
+               marker='o',
+               c=df['quality'].values,
+               s=30,
+               alpha=0.8,
+)
 plt.show()
 
-""" Example Model """
+
+""" Split Datset """
 X = df[x_columns]
 y = df[y_column]
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.3,
-                                                    # random_state=2,
-                                                    )
+                                                    random_state=2,
+                                                   )
 
+
+""" Linear Regression
+No promising results.
+"""
 model = LinearRegression()
-# model = BayesianRidge(n_iter=300, tol=0.001, fit_intercept=True)
 model.fit(X_train, y_train)
 
+print('\nLineare Regression:')
 print(f'R2 Trainingsdaten: {model.score(X_train, y_train)}')
 print(f'R2 Testdaten: {model.score(X_test, y_test)}')
+print('\n')
 
-# Polynomial
-# Grad 3: overfitting
 
-poly = PolynomialFeatures(degree=2)
-X_train_p = poly.fit_transform(X_train)
-X_test_p = poly.fit_transform(X_test)
-model_poly = LinearRegression()
-model_poly.fit(X_train_p, y_train)
-print(f'Polynomial: R2 Trainingsdaten: {model_poly.score(X_train_p, y_train)}')
-print(f'Polynomial: R2 Testdaten: {model_poly.score(X_test_p, y_test)}')
+""" Polynomial Regression
+No promising results.
+Overfitting starting with degree 3.
+"""
+for degree in [2,3]:
+    poly = PolynomialFeatures(degree=degree)
+    X_train_p = poly.fit_transform(X_train)
+    X_test_p = poly.fit_transform(X_test)
+    model = LinearRegression()
+    model.fit(X_train_p, y_train)
 
-# Decision Tree
+    print(f'Polynomielle Regression (grad={degree}):')
+    print(f'Polynomial: R2 Trainingsdaten: {model.score(X_train_p, y_train)}')
+    print(f'Polynomial: R2 Testdaten: {model.score(X_test_p, y_test)}')
+    print('\n')
 
-# model = DecisionTreeClassifier(min_samples_split=0.1)
-model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None,
-                               min_samples_split=3, max_features=None, random_state=None,
-                               min_impurity_decrease=0.0)
-# model = SVR(kernel='rbf', degree=2, max_iter=-1)
-model.fit(X_train, y_train)
+
+""" Bayesian Ridge Regression
+No promising results.
+"""
+model = BayesianRidge(n_iter=300, tol=0.001, fit_intercept=True)
+model.fit(X_train, y_train.values.ravel())
+
+print('Bayes\'sche lineare Regression:')
 print(f'R2 Trainingsdaten: {model.score(X_train, y_train)}')
 print(f'R2 Testdaten: {model.score(X_test, y_test)}')
+print('\n')
+
+
+""" Support Vector Regression """
+for kernel in ['rbf', 'linear', 'poly']:
+    model = SVR(kernel=kernel, degree=3, max_iter=-1)
+    model.fit(X_train, y_train.values.ravel())
+
+    print(f'Support Vector Regression (kernel={kernel}):')
+    print(f'Polynomial: R2 Trainingsdaten: {model.score(X_train, y_train)}')
+    print(f'Polynomial: R2 Testdaten: {model.score(X_test, y_test)}')
+    print('\n')
+
+
+""" Descision Tree Regression """
+for criterion in ['entropy', 'gini']:
+    model = DecisionTreeClassifier(criterion=criterion,
+                                   splitter='best',
+                                   max_depth=None,
+                                   min_samples_split=0.05,
+                                   max_features=None,
+                                   random_state=None,
+                                   min_impurity_decrease=0.01,
+                                  )
+    model.fit(X_train, y_train)
+    print(f'Entscheidungsbaum Regression (criterion={criterion})')
+    print(f'R2 Trainingsdaten: {model.score(X_train, y_train)}')
+    print(f'R2 Testdaten: {model.score(X_test, y_test)}')
+    print('\n')
+
+# Only plot last/best tree
+dot = export_graphviz(model, out_file=None,filled=True, feature_names=X.columns)
+graph = gv.Source(dot)
+graph.view()
+
+
+""" KNN """
+# TODO
