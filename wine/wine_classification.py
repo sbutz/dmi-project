22e3b509add3a880c013f458847482df4f47e340
Stefan Butz <stefan260699@gmail.com>
2022-01-12 08:31:23 +0100

    use decision tree classifier

--- wine/wine_classification.py
+++ wine/wine_classification.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from re import L
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
 
@@ -113,6 +115,46 @@ def knn_categories(df, x_columns, y_column, random_state, state):
     # print(knc.predict(X_test))
     # print(y_test)
 
+def tree_categories(df, x_columns, y_column, random_state, state):
+    df = df.copy(deep=True)
+
+    if state == 2:
+        df.loc[df.quality < 7, "quality"] = 0
+        df.loc[df.quality >= 7, "quality"] = 1
+        print("\nKDescion Tree with 2 categories(0-6/7-10):")
+    elif state == 3:
+        df.loc[df.quality < 6, "quality"] = 0
+        df.loc[df.quality >= 6, "quality"] = 1
+        df.loc[df.quality >= 8, "quality"] = 2
+        print("\nDescion Tree with 3 categories(0-5/6-7/8-10):")
+    else:
+        print("Invalid state")
+
+    X = df[x_columns]
+    y = df[y_column]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.3,
+                                                        random_state=random_state,
+                                                        )
+
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    knc = DecisionTreeClassifier(
+        criterion='gini',
+        splitter='best',
+        min_samples_split=0.05,
+        random_state=random_state,
+    )
+
+    knc.fit(X_train, y_train.values.ravel())
+    print('Accuracy of decision tree classifier on training set: {:.2f}'
+          .format(knc.score(X_train, y_train.values.ravel())))
+    print('Accuracy of decision tree classifier on test set: {:.2f}'
+          .format(knc.score(X_test, y_test.values.ravel())))
+
 
 def main():
     df_red = pd.read_csv('winequality-red-filtered.csv', sep=';', header=0)
@@ -137,6 +179,8 @@ def main():
 
     knn_categories(df_red, x_columns, y_column, random_state, 2)
     knn_categories(df_red, x_columns, y_column, random_state, 3)
+    tree_categories(df_red, x_columns, y_column, random_state, 2)
+    tree_categories(df_red, x_columns, y_column, random_state, 3)
 
 
 if __name__ == "__main__":
